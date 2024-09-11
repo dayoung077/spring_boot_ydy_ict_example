0dcf43f470be37dcbf07c04c8c36dae1f0380ecb
WIN-J2TB9CU2OL5\ICT02_15 <yoonda000@gmail.com>
2024-09-11 11:35:55 +0900

    첫 번째 커밋 - 스프링 기본 설정

---
+++ src/main/java/edu/ict/ex/SpringBootYdyIctExampleApplication.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootYdyIctExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootYdyIctExampleApplication.class, args);
+	}
+
+}
